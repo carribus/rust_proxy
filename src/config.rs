21f4a5fb774e460b40f746925288efd28071151d
carribus <carribus@gmail.com>
2019-08-15 23:10:17 +0200

    Implemented basic config loading and reading

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,24 @@
-pub struct Config {
-    
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+use serde_json::{Value};
+
+#[derive(Debug)]
+pub struct Config(Value);
+
+impl Config {
+
+    pub fn new(filename: &str) -> Result<Config, Box<dyn Error>> {
+        let mut file = File::open(filename)?;
+        let mut buffer = String::new();
+
+        file.read_to_string(&mut buffer)?;
+        let json = serde_json::from_str(&buffer)?;
+
+        Ok(Config(json))
+    }
+
+    pub fn value(&self) -> &Value {
+        &self.0
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 mod config;
 
 use std::error::Error;
+use std::env;
+use colored::*;
 
 use crate::config::Config;
 
+const CONFIG_FILENAME: &str = "config/config.json";
+
 fn main() {
     if let Err(err) = run_main() {
-        println!("ERROR: {}", err);
+        println!("{} {}", "ERROR: ".bright_red(), err);
     }
 }
 
 fn run_main() -> Result<(), Box<dyn Error>> {
-    let config = Config {};
+    let path = env::current_dir()?;
+    let config = Config::new(&format!("{}/{}", path.to_str().unwrap(), CONFIG_FILENAME))?;
+
+    println!("config = {:#?}", config);
+    println!("config.o.key = {:?}", config.value()["o"]["key"]);
 
     Ok(())
 }
