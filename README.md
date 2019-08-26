660f4a06a68a05f2fda23bf9efa4f233d0e1f8e6
carribus <carribus@gmail.com>
2019-08-26 16:02:35 +0200

    Rudimentary implementation of tunnel setup. Added README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# rust_proxy
+
+*WARNING* This project contains code that was never intended to be used as anything more than a learning exercise for myself.
+
+With this in mind, the idea behind this was to build a small proxy server in Rust that was (initially) able to proxy HTTP(S) calls. The code in this repo should _always_ be considered unstable and unusable :)
--- src/server.rs
+++ src/server.rs
@@ -1,22 +1,43 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::net::{TcpListener, TcpStream};
+use std::rc::Rc;
+use std::cell::{RefCell};
 
 use colored::*;
 
-use crate::http::{Request, RequestMethod};
+use crate::http::*; //{Request, RequestMethod};
 
 const READ_BUFFER_SIZE: usize = 8096;
 
+#[derive(Debug)]
+struct ConnectError;
+
+impl std::fmt::Display for ConnectError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Proxy CONNECT error handling error")
+    }
+}
+
+impl Error for ConnectError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+
 pub struct Server {
     listener: TcpListener,
+    connections: Rc<RefCell<HashMap<String, TcpStream>>>
 }
 
 impl Server {
     pub fn new(host: &str, port: u16) -> Result<Server, Box<dyn Error>> {
         let server = Server {
             listener: TcpListener::bind(format!("{}:{}", host, port))?,
+            connections: Rc::new(RefCell::new(HashMap::new())),
         };
         Ok(server)
     }
@@ -25,30 +46,12 @@ impl Server {
         for stream in self.listener.incoming() {
             match stream {
                 Ok(stream) => {
-                    let peer_addr = stream.peer_addr();
-                    println!("Incoming connection from {:?}", peer_addr);
-
-                    let mut reader = BufReader::new(stream);
-                    let mut buf_str = self.read_headers(&mut reader)?;
-
-                    if let Ok(req) = Request::from(&buf_str) {
-                        let mut req = req;
-                        println!("{}\nRequest: {:?}", "-".repeat(80), req);
-                        
-                        // if there is a body, read it
-                        if req.header_exists("Content-Length") {
-                            buf_str.clear();
-                            buf_str = self.read_body(&req, &mut reader)?;
-                            req.set_body(Some(buf_str));
-                        }
-
-                        if let Err(e) = self.proxy_request(&req) {
-                            println!("{} {}", "ERROR: ".bright_yellow(), e);
-                        }
+                    if let Err(e) = self.handle_incoming_connection(&stream) {
+                        println!("{} {}", "ERROR:".bright_yellow(), e);
                     }
                 }
-                Err(_e) => {
-                    println!("Connection failed");
+                Err(e) => {
+                    println!("{} {}", "ERROR: Listener connection failed".bright_red(), e);
                 }
             }
         }
@@ -56,64 +59,113 @@ impl Server {
         Ok(())
     }
 
-    fn read_headers(&self, reader: &mut dyn BufRead) -> Result<String, Box<dyn Error>> {
-        let mut buf = String::new();
+    fn handle_incoming_connection(&self, stream: &TcpStream) -> Result<(), Box<dyn Error>> {
 
-        loop {
-            let mut line = String::new();
-            let bytes_read = reader.read_line(&mut line)?;
-
-            if bytes_read == 0 || line == "\r\n" {
-                break;
-            }
-            buf.push_str(&line);
-        }
-
-        Ok(buf)
-    }
-
-    fn read_body(&self, req: &Request, reader: &mut dyn BufRead) -> Result<String, Box<dyn Error>> {
-        let mut buffer = [0; READ_BUFFER_SIZE];
-        let mut body = String::new();
-
-        if let Some(content_length) = req.get_header("Content-Length") {
-            let content_length = content_length.parse::<usize>()?;
-            let mut content_counter = 0;
-
-            println!("Reading {} bytes of content: ", content_length);
+        fn read_headers(reader: &mut dyn BufRead) -> Result<String, Box<dyn Error>> {
+            let mut buf = String::new();
 
             loop {
-                content_counter += reader.read(&mut buffer)?;
-                body.push_str(&String::from_utf8_lossy(&buffer[..]));
-                if content_counter >= content_length {
+                let mut line = String::new();
+                let bytes_read = reader.read_line(&mut line)?;
+
+                if bytes_read == 0 || line == "\r\n" {
                     break;
                 }
+                buf.push_str(&line);
             }
 
-            println!("Done reading body: {}", body);
+            Ok(buf)
         }
 
-        Ok(body)
+        fn read_body(req: &Request, reader: &mut dyn BufRead) -> Result<String, Box<dyn Error>> {
+            let mut buffer = [0; READ_BUFFER_SIZE];
+            let mut body = String::new();
+
+            if let Some(content_length) = req.get_header("Content-Length") {
+                let content_length = content_length.parse::<usize>()?;
+                let mut content_counter = 0;
+
+                // println!("Reading {} bytes of content: ", content_length);
+
+                loop {
+                    content_counter += reader.read(&mut buffer)?;
+                    body.push_str(&String::from_utf8_lossy(&buffer[..]));
+                    if content_counter >= content_length {
+                        break;
+                    }
+                }
+
+                // println!("Done reading body: {}", body);
+            }
+
+            Ok(body)
+        }
+
+
+        if let Ok(peer_addr) = stream.peer_addr() {
+            println!("Incoming connection from {:?}", peer_addr);
+        }
+
+        let mut req = {
+            let mut reader = BufReader::new(stream);
+            let mut buf_str = read_headers(&mut reader)?;
+            let mut req = Request::from(&buf_str)?;
+
+            // if there is a body, read it
+            if req.header_exists("Content-Length") {
+                buf_str.clear();
+                buf_str = read_body(&req, &mut reader)?;
+                req.set_body(Some(buf_str));
+            }
+
+            println!("Request: {:?}", req);
+            req
+        };
+
+        let mut stream = stream;
+        if let Err(e) = self.proxy_request(&req, &stream) {
+            println!("{} {}", "ERROR: ".bright_yellow(), e);
+        }
+
+        // stream.write("200 OK".as_bytes())?;
+
+        Ok(())
     }
 
-    fn proxy_request(&self, req: &Request) -> Result<(), Box<dyn Error>> {
+    fn proxy_request(&self, req: &Request, stream: &TcpStream) -> Result<(), Box<dyn Error>> {
         println!("Proxying request.. preparing connection");
-        let mut client = TcpStream::connect(req.uri())?;
 
-        println!("Selecting method...");
         match &req.method() {
             RequestMethod::CONNECT => {
-                println!("Sending CONNECT request");
+                let client = self.handle_connect_request(&req, stream)?; 
+                // TODO: You left off here... need to inform the source connection that the tunnel is established
             },
-            RequestMethod::GET => {
-                println!("Sending GET request");
-            },
-            RequestMethod::POST => {
-                println!("Sending POST request");
-            },
+            // RequestMethod::GET => {
+            //     println!("Sending GET request");
+            // },
+            // RequestMethod::POST => {
+            //     println!("Sending POST request");
+            // },
             _ => unimplemented!(),
         }
 
         Ok(())
     }
+
+    fn handle_connect_request(&self, req: &Request, stream: &TcpStream) -> Result<TcpStream, Box<dyn Error>> {
+        // println!("Handling CONNECT request: {:?}", req);
+        let mut conn_map = self.connections.borrow_mut();
+
+        if !conn_map.contains_key(req.uri()) {
+            println!("Creating new connection for request: {:?}", req);
+            let client = TcpStream::connect(req.uri())?;
+            conn_map.insert(req.uri().clone(), client.try_clone()?);
+        }
+
+        match conn_map.get(req.uri()) {
+            Some(client) => Ok(client.try_clone()?),
+            None => Err(Box::new(ConnectError))
+        }
+    }
 }
+
